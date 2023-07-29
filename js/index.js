1f9be4a9f6ab922f4643c45b167d1233a8e0a3a7
Tardfyou <3143596095@qq.com>
2023-07-29 15:41:35 +0800

    successful

---
+++ js/index.js
@@ -0,0 +1,24 @@
+﻿/*
+ * @Author: your name
+ * @Date: 2021-03-25 09:07:37
+ * @LastEditTime: 2021-03-25 09:12:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \tiny-heart-master\birthday-mobile\js\index.js
+ */
+$("#login-button").click(function (event) {
+  var userName = document.getElementById("userName").value;
+  var pwd = document.getElementById("pwd").value;
+  //修改密码请改此处
+  if (userName == "赵婷玉" && pwd == "040514") {
+    event.preventDefault();
+    $("form").fadeOut(500);
+    $(".wrapper").addClass("form-success");
+    setTimeout(function () {
+      location.href = "BirthdayCake.html";
+    }, 2000);
+  } else {
+    alert("Wrong Password");
+  }
+});
+
